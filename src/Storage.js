8ef82f0879d5dc26b1af082f27f2320a43f491a0
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-05-05 11:08:40 +0900

    *

--- src/Storage.js
+++ src/Storage.js
@@ -1,33 +1,6 @@
 let chrome = require('sinon-chrome');
 
-let createApiClass = require('./Api');
-// let createApiClass = require('./createApiClass');
-
-// class StorageArea extends Api {
-//   constructor(deferred, promise, storage_type) {
-//     super(
-//       deferred, promise, chrome.storage[storage_type],
-//       ['get', 'getBytesInUse', 'set', 'remove', 'clear'], []
-//     );
-//   }
-// }
-// 
-// 
-// 
-// class Storage extends Api {
-//   constructor(deferred, promise) {
-//     super(deferred, promise, chrome.storage, [], ['onChanged']);
-//     let self = this;
-// 
-//     ['sync', 'local', 'managed'].filter(t => t in chrome.storage)
-//     .forEach(storage_type => {
-//       self[storage_type] = new StorageArea(deferred, promise, storage_type);
-//     });
-//   }
-// }
-// 
-// 
-// module.exports = Storage;
+let createApiClass = require('./createApiClass');
 
 let params = [['get', 'getBytesInUse', 'set', 'remove', 'clear']];
 
--- src/createApiClass.js
+++ src/createApiClass.js
@@ -1,15 +1,30 @@
-let Api = require('./Api');
+let appendMethods = require('./appendMethods');
+let Event = require('./Event');
 
 
-{
-  api: api,
-  method_names: method_names,
-  event_names: event_names,
-  children: {
-    name: name
-    method_names: method_names,
-    event_names: event_names,
+function createApiClass(
+  api, method_names, event_names=[], children_params={}
+) {
+  class Api {
+    constructor(deferred, promise) {
+      let self = this;
+      appendMethods(deferred, promise, this, api, method_names);
+      event_names.filter(event_name => event_name in api)
+      .forEach(event_name => {
+        self[event_name] = new Event(deferred, promise, api[event_name]);
+      });
+      Object.keys(children_params).filter(child_name => child_name in api)
+      .forEach(child_name => {
+        let child_params = children_params[child_name];
+        let child = createApiClass.apply(
+          null, [api[child_name]].concat(child_params)
+        );
+        self[child_name] = new child(deferred, promise);
+      });
+    }
   }
+  
+  return Api;
 }
 
 
